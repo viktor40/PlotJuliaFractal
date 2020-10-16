3181e955e062edfa73936716c1625d811659f706
viktor40 <vannieuwenhuize.viktor@gmail.com>
2020-10-17 01:09:41 +0200

    initial commit

--- julia.py
+++ julia.py
@@ -6,7 +6,6 @@ Various comments are placed in places where you can change variables to change t
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 # Set the size of the image (fractal doesn't scale with the sizes, so changing the size might mean some parts of the
 # fractal get cut off
 m = 480
@@ -17,12 +16,8 @@ x = np.linspace(-m / s, m / s, num=m).reshape((1, m))
 y = np.linspace(-n / s, n / s, num=n).reshape((n, 1))
 Z = np.tile(x, (n, 1)) + 1j * np.tile(y, (1, m))
 
-# this is the constant used in the complex polynomials
-c = 0.1 + 0.651j
-
-# C is the matrix representing the (complex) constant part of the polynomials
-C = np.full((n, m), c)
-
+c = 0.1 + 0.651j  # this is the constant used in the complex polynomials
+C = np.full((n, m), c)  # C is the matrix representing the (complex) constant part of the polynomials
 M = np.full((n, m), True, dtype=bool)
 N = np.zeros((n, m))
 
@@ -40,6 +35,6 @@ ax.set_yticks([])
 
 # you can change the color map (cmap) to different maps for different colours in hte plot
 # more color maps can be found at https://matplotlib.org/3.3.2/tutorials/colors/colormaps.html
-plt.imshow(np.flipud(N), cmap='inferno')
+plt.imshow(np.flipud(N), cmap='plasma')
 plt.savefig('julia.png')
 plt.show()
