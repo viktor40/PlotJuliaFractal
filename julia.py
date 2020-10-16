8307b49eb68dbcd1951f87b80fc6e836a66b0b4d
viktor40 <vannieuwenhuize.viktor@gmail.com>
2020-10-17 01:06:25 +0200

    initial commit

--- julia.py
+++ julia.py
@@ -17,9 +17,11 @@ x = np.linspace(-m / s, m / s, num=m).reshape((1, m))
 y = np.linspace(-n / s, n / s, num=n).reshape((n, 1))
 Z = np.tile(x, (n, 1)) + 1j * np.tile(y, (1, m))
 
-C = np.full((n, m),
-            0.1 + 0.651j  # change this to change the constants in the set
-            )
+# this is the constant used in the complex polynomials
+c = 0.1 + 0.651j
+
+# C is the matrix representing the (complex) constant part of the polynomials
+C = np.full((n, m), c)
 
 M = np.full((n, m), True, dtype=bool)
 N = np.zeros((n, m))
@@ -38,8 +40,6 @@ ax.set_yticks([])
 
 # you can change the color map (cmap) to different maps for different colours in hte plot
 # more color maps can be found at https://matplotlib.org/3.3.2/tutorials/colors/colormaps.html
-plt.imshow(np.flipud(N), cmap='viridis')
+plt.imshow(np.flipud(N), cmap='inferno')
 plt.savefig('julia.png')
 plt.show()
-
-
